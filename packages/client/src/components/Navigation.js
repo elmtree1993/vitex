73f659880fcaa1cd2723272d768d23f005620113
Ethan Morrison <elmtree1993@yahoo.com>
2023-08-09 22:52:17 -0400

    first commit

---
+++ packages/client/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import {Navbar, Nav} from "react-bootstrap"
+
+const Navigation = (props) => {
+  return (
+    <Navbar>
+      <Navbar.Brand as="h3">My Navbar</Navbar.Brand>
+      <Nav>
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/products">All Products</Nav.Link>
+      <Nav.Link as={Link} to="/about">About Me</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default Navigation;
---
+++ packages/client/src/resume.js
@@ -0,0 +1,110 @@
+const jobs = [
+  {
+    employer: "Gordon Marketing",
+    dates: "",
+    title: "",
+    bullets: ["", "", ""],
+  },
+  {
+    employer: "Wardwell Trucking",
+    dates: "October 2022-July 2023",
+    title: "truck driver",
+    overview:
+      "I drove Class A and B commercial trucks delivering goods and materials to and from jobsites. I was responsible for daily inspections, maintenance, and safe operation of heavy trucks according to company policy and state regulations.",
+    bullets: [
+      "Ensured company policies and procedures were followed",
+      "Escalated preventative maintenance needs to management",
+      "Identified areas of opportunity for process improvement such as: utilization of digital data transfer for time and record keeping vs paper tickets and hand delivery",
+    ],
+  },
+  {
+    employer: "Elm Tree Operating Solutions",
+    dates: "January 2019-November 2022",
+    title: "Owner/Operator",
+    overview:
+      "I was the Owner/Operator of a small start up business that specialized in heavy equipment, commercial trucking, and residential construction. I was responsible for administrative duties such as accounts receivable/payable and equipment acquisition. I transported, operated, and maintained various resources and handled customer acquisition, logistics and scheduling.",
+    bullets: [
+      "Demonstrated willingness to take on additional responsibilities to help other teams be successful",
+      "ssertive and empathetic approach to ensuring safety policies and procedures are followed",
+      "Nurture relationships with both internal and external customers in order to ensure business success",
+      "Maintain multiple projects at a given time, ensuring production goals are met",
+    ],
+  },
+  {
+    employer: "Custom Concrete",
+    dates: "October 2012-October 2016, November 2018-January 2021",
+    title: "Department Lead",
+    overview:
+      "I was a surveyor and Department Lead responsible for general management, scheduling, and onboarding of employees. I was responsible for meeting with builders ensuring plans and specifications meet customer expectations.",
+    bullets: [
+      "Prioritize and schedule upcoming projects, while coordinating with adjacent departments to minimize projects constraints",
+      "Leverage effective communication techniques in order to ensure project success",
+      "Train new hires to ensure consistency in operations",
+      "Developed and standardized practices to ensure effective utilization of company resources",
+      "Built relationships across organization in order to improve cross-functional alignment and efficiency",
+      "Leveraged technical knowledge to streamline production and cut costs",
+      "Prioritize and schedule upcoming projects, while coordinating with adjacent departments to minimize project constraints",
+    ],
+  },
+  {
+    employer: "Kirby Drywall",
+    dates: "July 2018-November 2018",
+    title: "Carpenter",
+    overview:
+      "I was a commercial carpenter for a small framing contractor specializing in warehouse interior buildouts. I was responsible for self starting projects, giving inventory of time and materials, and accounting for production rate each day. Coordination with general contractor and adjacent teams were crucial to maintain work flow and maximize productivity.",
+    bullets: [
+      "Completed projects on time and under budget while operating within regulatory constraints",
+      "Utilize proactive mindset to ensure efficient workflow transition between groups",
+      "Ensure prompt and accurate delivery of goods and materials to multiple projects at any given time",
+    ],
+  },
+  {
+    employer: "Advanced Interior Construction",
+    dates: "May 2017-July 2018",
+    title: "Carpenter",
+    overview:
+      "I was a commercial carpenter for a large framing contractor specializing in large scale new construction and largescale commercial remodeling. I was responsible for starting crews consistently on projects, relaying material inventory and team needs to complete projects according to company policy and procedures. My teams successfully completed multiple projects before deadlines.",
+    bullets: [
+      "Follow strict processes and guidelines to ensure quality of project",
+      "Utilized effective communication, precise speech, and active listening when working with upper management on possible road blocks and discrepancies",
+      "Operated as project lead on most projects including production, training, and onboarding",
+      "Communicated with both internal and external clients to arrive at the most advantageous outcome",
+    ],
+  },
+  {
+    employer: "Indiana Renovations, Inc.",
+    dates: "March 2017-May 2017",
+    title: "General Laborer",
+    overview:
+      "I was a general laborer and tile apprentice for a small contractor specializing in residential kitchen and bathroom remodels. I was responsible for assisting the project lead throughout the project from conception to delivery. ",
+    bullets: [
+      "Maintain a manner of professionalism, courtesy, and respect for customer and the work environment",
+      "Strict adherence to company policy and procedures while learning from leads.",
+      "Leverage opportunities to grow skills in order to achieve higher productivity",
+    ],
+  },
+  {
+    employer: "Schoettmer Prime Pork",
+    dates: "November 2016-March 2017",
+    title: "General Laborer",
+    overview:
+      "I was a general laborer and mill operator on a local farrow-to-finish pig farm. I was responsible for manufacture and delivery of feed, general maintenance of facilities, equipment and vehicles.",
+    bullets: [
+      "Deliver and distribute finished product while operating under strict deadlines and constraints",
+      "Demonstrated willingness to take on additional responsibilities to help other teams be successful",
+    ],
+  },
+  {
+    employer: "Dick's Sporting Goods",
+    dates: "October 2011-April 2013",
+    title: "Sales Associate",
+    overview:
+      "I was an Outdoors sales associate at a chain retail store. I was responsible for customer service and sales, inventory of stock and restocking of the sales floor.",
+    bullets: [
+      "Contributed to a growth of knowledge among customers and peers",
+      "Excelled at multitasking and prioritizing while maintaining presence and focus on the sales floor",
+      "Leveraged effective communication to build relationships with customers to ensure repeat business",
+      "Worked directly with management to implement more effective sales tactics that increased department revenue based on current sales goals",
+    ],
+  },
+];
